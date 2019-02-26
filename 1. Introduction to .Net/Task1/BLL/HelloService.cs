866b29a867eff333936acb4208811588d3d7dff9
Anton Plotnikau <Anton_Plotnikau@epam.com>
2019-02-26 12:21:13 +0300

    Added Standart .Net library

---
+++ 1. Introduction to .Net/Task1/BLL/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BLL
+{
+    public class HelloService
+    {
+		public string SayHello(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+			{
+				return $"{DateTime.Now} Hello, Anonym!";
+			}
+
+			return $"{DateTime.Now} Hello, {name}!";
+		}
+    }
+}
