bc3f428e13656457ff0b21637e09c75f86b0d9ca
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-02-28 01:07:33 +0300

    Added .Net Core MVC project with Docker support

---
+++ 1. Introduction to .Net/Task1/CoreWebUI/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using BLL;
+
+namespace CoreWebUI.Controllers
+{
+	public class HelloController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult SayHello(string name)
+		{
+			string response = HelloService.SayHello(DateTime.Now, name);
+
+			return Content(response);
+		}
+	}
+}
