f472e8080bf407c71c3845570107a53b936f219c
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-02-28 20:58:13 +0300

    Added .net framework console and asp.net apps

---
+++ 1. Introduction to .Net/Task1/FrameworkConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using BLL;
+
+namespace FrameworkConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = args.Length > 0 ? args[0] : null;
+			string output = HelloService.SayHello(DateTime.Now, name);
+			Console.WriteLine(output);
+		}
+	}
+}
---
+++ 1. Introduction to .Net/Task1/FrameworkWebUI/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+using BLL;
+
+namespace FrameworkWebUI.Controllers
+{
+    public class HelloController : Controller
+    {
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult SayHello(string name)
+		{
+			string response = HelloService.SayHello(DateTime.Now, name);
+
+			return Content(response);
+		}
+	}
+}
