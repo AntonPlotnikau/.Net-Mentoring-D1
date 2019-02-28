19fc6676aecf780bc7433c5a72440d3f05d6438f
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-02-28 23:47:10 +0300

    Added xamarin android and uwp projects

---
+++ 1. Introduction to .Net/XamarinFormsUI/XamarinFormsUI/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using BLL;
+using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsUI
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnButtonClicked(object sender, EventArgs e)
+		{
+			string name = this.helloEntry.Text;
+			this.helloLabel.Text = HelloService.SayHello(DateTime.Now, name);
+		}
+	}
+}
