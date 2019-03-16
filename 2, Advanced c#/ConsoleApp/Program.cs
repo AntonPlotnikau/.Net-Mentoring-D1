10fc092da15ee8338109a52f02ec2cba5645997c
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-03-16 11:13:52 +0300

    added console app for fast testing

---
+++ 2, Advanced c#/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using FileSystemHelper.Models;
+using FileSystemHelper.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var visitor = new FileSystemVisitor(new DirectoryService(), (path) => path == @"D:\.Net-Mentoring-D1\.git\hooks");
+			visitor.FilteredDirectoryFinded += (o, e) => e.Status = SearchStatus.Skip;
+			visitor.DirectoryFinded += (o, e) => Console.WriteLine(e.Path);
+			visitor.GetDirectoryTree(@"‪‪D:\.Net-Mentoring-D1");
+		}
+	}
+}
