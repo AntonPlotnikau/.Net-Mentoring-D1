2f5e11784db2b9173a44cf5c06fcd612537ac5ed
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-04-27 20:11:55 +0300

    Added NoSql task

---
+++ 7. NoSql/BookStorage.Interface/Interfaces/IBooksRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookStorage.Interface.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace BookStorage.Interface.Interfaces
+{
+	public interface IBooksRepository
+	{
+		void AddBook(Book book);
+
+		void AddBooks(IEnumerable<Book> books);
+
+		IEnumerable<Book> GetBooks();
+
+		IEnumerable<Book> GetBooks(Func<Book, bool> predicate);
+
+		void UpdateBooks(UpdateDefinition<Book> updateDefinition, Expression<Func<Book, bool>> filter);
+
+		void UpdateBooks(UpdateDefinition<Book> updateDefinition);
+
+		void DeleteBooks();
+
+		void DeleteBooks(Expression<Func<Book, bool>> filter);
+
+		IEnumerable<TResult> Select<TResult>(Func<Book, TResult> selector);
+	}
+}
---
+++ 7. NoSql/BookStorage.Interface/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System.Collections.Generic;
+namespace BookStorage.Interface.Models
+{
+	public class Book
+	{
+		public ObjectId id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Author { get; set; }
+
+		public int Count { get; set; }
+
+		public IEnumerable<string> Genres { get; set; }
+
+		public int Year { get; set; }
+	}
+}
