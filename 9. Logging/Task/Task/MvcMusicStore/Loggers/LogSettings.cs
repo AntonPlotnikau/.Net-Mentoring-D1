035e0c3917a004e7903dc7f8a4de4bf893fe4061
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-05-18 23:53:54 +0300

    Added logging

---
+++ 9. Logging/Task/Task/MvcMusicStore/Loggers/LogSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Loggers
+{
+	public class LogSettings : ConfigurationSection
+	{
+		public static LogSettings Settings { get; } = ConfigurationManager.GetSection("LogSettings") as LogSettings;
+
+		[ConfigurationProperty("isEnabled", DefaultValue = true, IsRequired = false)]
+		public bool IsEnabled
+		{
+			get { return (bool)this["isEnabled"]; }
+			set { this["isEnabled"] = value; }
+		}
+
+		[ConfigurationProperty("path", DefaultValue = @"D:\logs.xml", IsRequired = false)]
+		public string Path
+		{
+			get { return (string)this["path"]; }
+			set { this["path"] = value; }
+		}
+	}
+}
